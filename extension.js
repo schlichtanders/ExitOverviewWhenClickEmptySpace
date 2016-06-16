4700d0959f4a5a091cce41526ba2b395db97e170
Stephan Sahm <Stephan.Sahm@gmx.de>
2016-06-16 18:35:20 +0200

    small changes

--- extension.js
+++ extension.js
@@ -1,11 +1,6 @@
 
 /*
- * Show Desktop from Overview
- * Very small extension which minimizes all windows when you click on an empty space in the windows-overview-mode.
- *
- * Contact:
- * https://github.com/Bazon/ShowDesktopFromOverview/tree/master or bazonbloch@arcor.de
- * Feel free to send improvments!
+ * Exit Overview when clicking on empty space
 */
 
 const Main = imports.ui.main;
@@ -14,24 +9,7 @@ const Shell = imports.gi.Shell;
 var connectid = null;
 var reactiveBefore = null;
 
-function _showDesktop() {
-    //TODO: when public, do that by meta_screen_show_desktop();
-    
-    let activeWorkspace = global.screen.get_active_workspace();
-    let tracker = Shell.WindowTracker.get_default();
-    let windows = activeWorkspace.list_windows();
-    for (let i = 0; i < windows.length; i++) {
-           //New in V3&4: tracker.is_window_interesting checks whether this is a real window and not a desktop icon
-           if (!windows[i].minimized && (windows[i].get_window_type() == 0) && !(windows[i].get_title().indexOf("Conky") >= 0)) {
-	       windows[i].minimize();
-           }
-    }
-}
-
 function _hideOverview() {
-    if(Main.overview.viewSelector._activePage == Main.overview.viewSelector._workspacesPage) {
-    	_showDesktop();
-    }
     Main.overview.hide();
 }
 function init() {
